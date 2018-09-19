f42661c48dde22a2639f7f48e8a04f51c61f1ea8
Bobby <blipco619@gmail.com>
2018-09-19 16:04:21 -0700

    9-19-18

---
+++ 9-19-18/fizzBuzz.js
@@ -0,0 +1,20 @@
+// Return an array
+function fizzbuzz(n) {
+  finishedArr = [];
+    for(let i=1;i<=n;i++) {
+      if(i%3==0 && i%5 ===0) {
+        finishedArr.push('FizzBuzz');
+      } else {
+        if(i%3==0) {
+          finishedArr.push('Fizz');
+        } else {
+          if(i%5==0) {
+            finishedArr.push('Buzz');
+          } else {
+            finishedArr.push(i);
+          };
+        };
+      };
+    };
+    return finishedArr;
+  };
